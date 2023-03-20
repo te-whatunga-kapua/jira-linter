6209c19a0e2fcdbdc13f4cc27553fa556917b700
Andrew Frazer <andrew.frazer@raindancers.cloud>
2023-03-21 10:55:21 +1300

    Checking

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,47 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import * as exec from "@actions/exec"
 
-function run() {
+
+export async function getCommitMessage(sha: string): Promise<string> {
+  let message = "";
+
+  const options = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        message += data.toString();
+      }
+    }
+  };
+
+  const args: string[] = ["rev-list", "--format=%B", "--max-count=1", sha];
+
+  await exec.exec("git", args, options);
+  message.trim();
+
+  return message;
+}
+
+async function run(): Promise<void> {
   const branch = github.context.payload.ref.split('/')[2]
   const jiraIssue = `${branch.split('-')[0]}-${branch.split('-')[1]}`
   const regex = new RegExp(`${jiraIssue}:`)
-  const title = 
-    github.context.payload &&
-    github.context.payload.pull_request &&
-    github.context.payload.pull_request.title
-  core.info(title)
-  const isValid = regex.test(title)
+  
+  const commitSHA = github.context.sha;
+  core.debug(`Commit Message SHA:${commitSHA}`);
+
+  const message = await getCommitMessage(commitSHA);
+  core.debug(`Commit Message Found:\n${message}`);
+  
+  const isValid = regex.test(message)
   if (!isValid) {
     core.setFailed(
-      `Pull request title: "\n${title}\n" does not contain the valid Jira Issue code for this Branch "${jiraIssue}".`,
+      `Commit Message: "\n${message}\n" does not contain the valid Jira Issue code for this Branch "${jiraIssue}".`,
     )
   }
 }
+
+
 
 run()
 
